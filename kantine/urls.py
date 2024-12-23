0f272e82e8dfea68fbf5fe86d99e82ab3b3d0f0a
Patrick Rauscher <prauscher@prauscher.de>
2024-12-23 10:08:06 +0100

    add missing import

--- kantine/urls.py
+++ kantine/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.views.generic import RedirectView
 from kantine.views import jwt_login
 
 urlpatterns = [
