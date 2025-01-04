bd8d73d025c05baf7b4f595de24a0306ff719dcb
Patrick Rauscher <prauscher@prauscher.de>
2025-01-04 22:53:26 +0100

    activate housekeeping

--- contrib/healthcheck.sh
+++ contrib/healthcheck.sh
@@ -3,4 +3,12 @@
 # use empty Host-header to hide message in access log
 curl -H "Host: " -s http://localhost:${PORT}/healthcheck/ > /dev/null || exit 1
 
-python3 /opt/app/manage.py send_hermine
+# run housekeeping only once a day
+if [ ! -f /tmp/_housekeeping ] || [ $(( $(date +%s) - $(stat -c "%Y" /tmp/_housekeeping) )) -gt 86400 ]; then
+	/housekeeping.sh
+	touch /tmp/_housekeeping
+else
+	# avoid timeout, so only run housekeeping or these jobs
+	python3 /opt/app/manage.py send_hermine
+fi
+
