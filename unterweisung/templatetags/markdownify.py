a6525dc8eef1f5a67dbad064ffdb9df6c52e3cc7
Patrick Rauscher <prauscher@prauscher.de>
2025-11-27 01:33:34 +0100

    make markdown inline

--- unterweisung/templatetags/markdownify.py
+++ unterweisung/templatetags/markdownify.py
@@ -8,3 +8,16 @@ register = template.Library()
 @register.filter("markdownify")
 def markdownify_filter(value: str) -> SafeText:
     return mark_safe(markdownify(value))
+
+
+@register.filter("markdownify_inline")
+def markdownify_inline_filter(value: str) -> SafeText:
+    markdown = markdownify(value)
+
+    # common case: python-markdown wraps everything in a single paragraph,
+    # leading to strange line breaks
+    if markdown.startswith("<p>") and "<p>" not in markdown[1:] and \
+       markdown.endswith("</p>") and "</p>" not in markdown[:-1]:
+        markdown = markdown[3:-4]
+
+    return mark_safe(markdown)
