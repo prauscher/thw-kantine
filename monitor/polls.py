d8849606480d3a0fc9dbc892e56ffdaa8e699f50
Patrick Rauscher <prauscher@prauscher.de>
2025-12-26 15:48:21 +0100

    monitor: try to minimize cache updates per call

--- monitor/polls.py
+++ monitor/polls.py
@@ -5,6 +5,6 @@ import requests
 from .models import CacheItem
 
 
-@CacheItem.cache(expiration=timedelta(minutes=5))
+@CacheItem.cache(expiration=timedelta(minutes=3))
 def query_polls(polls_url):
     return requests.get(polls_url, headers={"Accept": "application/json"}).json()["ocs"]["data"]["polls"]
