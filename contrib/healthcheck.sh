c89582cf1c0a3129f4687c61a274c1491e34380f
Patrick Rauscher <prauscher@prauscher.de>
2026-01-15 17:16:53 +0100

    use lockfile for background tasks

--- contrib/healthcheck.sh
+++ contrib/healthcheck.sh
@@ -7,8 +7,12 @@ curl -H "Host: " -s http://localhost:${PORT}/healthcheck/ > /dev/null || exit 1
 if [ ! -f /tmp/_housekeeping ] || [ $(( $(date +%s) - $(stat -c "%Y" /tmp/_housekeeping) )) -gt 86400 ]; then
 	/housekeeping.sh
 	touch /tmp/_housekeeping
-else
+elif [ ! -f /tmp/_background ]; then
+	touch /tmp/_background
+
 	# avoid timeout, so only run housekeeping or these jobs
 	python3 /opt/app/manage.py send_hermine
+
+	rm /tmp/_background
 fi
 
